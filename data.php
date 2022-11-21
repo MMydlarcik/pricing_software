b3c68433e8240417a176dc45c753c828f7cb2258
Milan Mydlarčík <milanmydlarcik@inove.cz>
2022-11-21 16:48:18 +0100

    Update data

---
+++ data.php
@@ -0,0 +1,200 @@
+<?php
+
+$data = [];
+
+
+$product = new Product('Samsung Galaxy Z Flip4 5G F721B 8GB/256GB', '8806094507386');
+$product->addConcurence('Datart',26091,0);
+$product->addConcurence('Mall',28990,0);
+$product->addConcurence('MobilPohotovost',28990,0);
+$product->addConcurence('CZC',28995,0);
+$product->addConcurence('Heureka',18949,0);
+$product->addConcurence('Zbozi',18940,0);
+$product->addConcurence('GoogleNakupy',20690,0);
+
+$product->addSource('Datart',26091,0, [29245,28122,28298]);
+$product->addSource('Mall',28990,0, [29205,28150,28300]);
+$product->addSource('MobilPohotovost',12343,0, [29105,28110,28154]);
+$product->addSource('Mironet',19603,0, [29205,28150,28300]);
+
+$data[] = $product;
+
+//
+
+$product = new Product('Apple iPhone 14 Pro 128GB', '1942534014902');
+$product->addConcurence('Datart',33490,15);
+$product->addConcurence('Mall',33490,30);
+$product->addConcurence('MobilPohotovost',28990,0);
+$product->addConcurence('CZC',33490,30);
+$product->addConcurence('Heureka',40164,0);
+$product->addConcurence('Zbozi',32514,30);
+$product->addConcurence('GoogleNakupy',29990,0);
+
+$product->addSource('Datart',33490,15, [35036,34815,35678]);
+$product->addSource('Mall',33490,30, [35000,34949,35678]);
+$product->addSource('MobilPohotovost',28990,0, [35099,34999,34900]);
+$product->addSource('Mironet',35660,0, [35900,34890,34900]);
+
+$data[] = $product;
+
+//
+
+$product = new Product('Samsung Galaxy S22 5G S901B 8GB/128GB', '8806092878617');
+$product->addConcurence('Datart',17991,0);
+$product->addConcurence('Mall',19990,0);
+$product->addConcurence('MobilPohotovost',19990,0);
+$product->addConcurence('CZC',19990,30);
+$product->addConcurence('Heureka',15649,0);
+$product->addConcurence('Zbozi',15549,0);
+$product->addConcurence('GoogleNakupy',15649,0);
+
+$product->addSource('Datart',17991,0, [21611,21465,20258]);
+$product->addSource('Mall',19990,0, [21500,21480,20300]);
+$product->addSource('MobilPohotovost',19990,0, [21650,21700,20830]);
+$product->addSource('Mironet',16490,0, [21800,21640,20439]);
+
+$data[] = $product;
+
+//
+
+$product = new Product('Samsung Galaxy A13 A137 4GB/64GB', '8806094387292');
+$product->addConcurence('Datart',4179,0);
+$product->addConcurence('Mall',3890,0);
+$product->addConcurence('MobilPohotovost',4179,0);
+$product->addConcurence('CZC',4179,0);
+$product->addConcurence('Heureka',4169,0);
+$product->addConcurence('Zbozi',3450,0);
+$product->addConcurence('GoogleNakupy',3890,0);
+
+$product->addSource('Datart',4179,0, [4684,4571,4518]);
+$product->addSource('Mall',3890,0, [4100,4580,4501]);
+$product->addSource('MobilPohotovost',4179,0, [4159,4571,4520]);
+$product->addSource('Mironet',4390,0, [4699,4520,4518]);
+
+$data[] = $product;
+
+//
+
+$product = new Product('Xiaomi Redmi Note 11 4GB/128GB', '6934177767333');
+$product->addConcurence('Datart',5289,0);
+$product->addConcurence('Mall',5289,0);
+$product->addConcurence('MobilPohotovost',5359,0);
+$product->addConcurence('CZC',5330,30);
+$product->addConcurence('Heureka',4848,0);
+$product->addConcurence('Zbozi',4195,0);
+$product->addConcurence('GoogleNakupy',4848,0);
+
+$product->addSource('Datart',5289,0, [5569,5585,5548]);
+$product->addSource('Mall',5289,0, [5540,5260,4638]);
+$product->addSource('MobilPohotovost',5359,0, [5565,4571,4701]);
+$product->addSource('Mironet',5106,0, [5555,4901,5284]);
+
+$data[] = $product;
+
+////
+
+$product = new Product('Samsung Galaxy Watch 4 40mm SM-R860', '8806094494082');
+$product->addConcurence('Datart',3490,4);
+$product->addConcurence('Mall',4490,0);
+$product->addConcurence('MobilPohotovost',3990,0);
+$product->addConcurence('CZC',4190,30);
+$product->addConcurence('Heureka',5544,0);
+$product->addConcurence('Zbozi',3490,0);
+$product->addConcurence('GoogleNakupy',3990,0);
+
+$product->addSource('Datart',3490,4, [5652,5325,5098]);
+$product->addSource('Mall',4490,0, [5493,5340,5100]);
+$product->addSource('MobilPohotovost',3990,0, [5490,5400,5001]);
+$product->addSource('Mironet',3886,0, [5642,5490,5010]);
+
+$data[] = $product;
+
+//
+
+$product = new Product('Apple Watch SE 40mm', '1942534151454');
+$product->addConcurence('Datart',6290,0);
+$product->addConcurence('Mall',7690,1);
+$product->addConcurence('MobilPohotovost',6690,0);
+$product->addConcurence('CZC',6990,0);
+$product->addConcurence('Heureka',6990,0);
+$product->addConcurence('Zbozi',5990,0);
+$product->addConcurence('GoogleNakupy',5990,0);
+
+$product->addSource('Datart',6290,0, [8687,8283,8263]);
+$product->addSource('Mall',7690,1, [8688,8230,8304]);
+$product->addSource('MobilPohotovost',6990,0, [8750,8299,8210]);
+$product->addSource('Mironet',7448,0, [8640,8245,8106]);
+
+$data[] = $product;
+
+//
+
+$product = new Product('Apple Watch Series 7 45mm', '1942525735250');
+$product->addConcurence('Datart',8890,0);
+$product->addConcurence('Mall',9490,0);
+$product->addConcurence('MobilPohotovost',9890,0);
+$product->addConcurence('CZC',11790,0);
+$product->addConcurence('Heureka',11790,0);
+$product->addConcurence('Zbozi',11690,0);
+$product->addConcurence('GoogleNakupy',8590,0);
+
+$product->addSource('Datart',8890,0, [16191,16379,13396]);
+$product->addSource('Mall',9490,0, [16100,15400,13990]);
+$product->addSource('MobilPohotovost',9890,0, [17465,16230,13501]);
+$product->addSource('Mironet',10990,0, [15490,16300,12568]);
+
+$data[] = $product;
+
+//
+
+$product = new Product('Xiaomi Watch S1 Active', '6934177755224');
+$product->addConcurence('Datart',2990,0);
+$product->addConcurence('Mall',3594,0);
+$product->addConcurence('MobilPohotovost',2999,0);
+$product->addConcurence('CZC',2999,0);
+$product->addConcurence('Heureka',2792,0);
+$product->addConcurence('Zbozi',2792,0);
+$product->addConcurence('GoogleNakupy',3998,0);
+
+$product->addSource('Datart',2990,0, [3750,3954,3607]);
+$product->addSource('Mall',3594,0, [3754,3920,3601]);
+$product->addSource('MobilPohotovost',2999,0, [3769,4002,3599]);
+$product->addSource('Mironet',3423,0, [3689,3967,3593]);
+
+$data[] = $product;
+
+//
+
+$product = new Product('Garmin vivoactive 4S', '7537592277082');
+$product->addConcurence('Datart',5390,0);
+$product->addConcurence('Mall',6790,0);
+$product->addConcurence('MobilPohotovost',5390,0);
+$product->addConcurence('CZC',5469,3);
+$product->addConcurence('Heureka',5280,0);
+$product->addConcurence('Zbozi',4790,0);
+$product->addConcurence('GoogleNakupy',4790,0);
+
+$product->addSource('Datart',5390,0, [6822,6950,6661]);
+$product->addSource('Mall',6790,0, [6801,6955,6799]);
+$product->addSource('MobilPohotovost',5390,0, [6600,6911,6493]);
+$product->addSource('Mironet',5161,0, [6854,6999,6749]);
+
+$data[] = $product;
+
+////
+
+$product = new Product('Apple Macbook Air 2020', '1942520560352');
+$product->addConcurence('Datart',26990,0);
+$product->addConcurence('Mall',26490,0);
+$product->addConcurence('MobilPohotovost',26490,0);
+$product->addConcurence('CZC',25790,3);
+$product->addConcurence('Heureka',26990,0);
+$product->addConcurence('Zbozi',4790,0);
+$product->addConcurence('GoogleNakupy',4790,0);
+
+$product->addSource('Datart',26990,0, [6822,6950,6661]);
+$product->addSource('Mall',26490,0, [6801,6955,6799]);
+$product->addSource('MobilPohotovost',26490,0, [6600,6911,6493]);
+$product->addSource('Mironet',28990,0, [6854,6999,6749]);
+
+$data[] = $product;
